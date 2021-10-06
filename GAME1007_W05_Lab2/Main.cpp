e29eb7587bb6fde5ca196e084803cb11480a72da
TheProfezzzor <TheProfezzzor@users.noreply.github.com>
2021-10-06 09:52:05 -0400

    Added main, Run and Init functions.

--- GAME1007_W05_Lab2/Main.cpp
+++ GAME1007_W05_Lab2/Main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+using namespace std;
 
-
-
-int main()
+// Init function. Sets up SDL and all subsystems.
+void Init()
 {
+	cout << "Initializing game..." << endl;
+}
+
+// Run function. Contains the game loop.
+int Run()
+{
+	bool isRunning = true;
+	char input;
+
+	Init();
+	while (isRunning) // This is our game loop.
+	{
+		
+	}
 
 	return 0;
+}
+
+// main function. Entry point of every C++ program.
+int main()
+{
+	return Run();
 }
